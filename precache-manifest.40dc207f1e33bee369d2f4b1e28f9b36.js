6629d5cf0089991d5cf660fd4c27d2ac9ccd351a
myusername <salguerocuentas@gmail.com>
2020-10-24 21:42:39 -0300

    Updates

---
+++ precache-manifest.40dc207f1e33bee369d2f4b1e28f9b36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e7f92a7bc6a0a8915b65023eecff4d9",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "40582fbdf952ffd0f419",
+    "url": "/test/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3c337af0966a96ec6fa9",
+    "url": "/test/static/css/main.5c4645da.chunk.css"
+  },
+  {
+    "revision": "40582fbdf952ffd0f419",
+    "url": "/test/static/js/2.d7f7f0b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.d7f7f0b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c337af0966a96ec6fa9",
+    "url": "/test/static/js/main.e69e27fb.chunk.js"
+  },
+  {
+    "revision": "ce7d5482e0b360dee444",
+    "url": "/test/static/js/runtime-main.85fc4ea0.js"
+  }
+]);
