ce34cb8f5140398936f61f3d7e91fe3b11f502df
myusername <salguerocuentas@gmail.com>
2020-10-25 00:32:34 -0300

    added 4th project

--- src/resumeData.js
+++ src/resumeData.js
@@ -9,7 +9,7 @@ let resumeData = {
   socialLinks: [
     {
       name: "LinkedIn",
-      link: "www.linkedin.com/in/salgueroagustin",
+      link: "https://www.linkedin.com/in/salgueroagustin/",
       icon: "fab fa-linkedin",
     },
     {
@@ -34,9 +34,15 @@ let resumeData = {
     },
     {
       modalLink: "#projectModal3",
-      projectCover: "/images/projects/Project3.png",
+      projectCover: "./images/projects/Project3.png",
       captionHeading: "Delilah Resto",
       captionSub: "NodeJS project",
+    },
+    {
+      modalLink: "#projectModal4",
+      projectCover: "./images/projects/Project4.png",
+      captionHeading: "Portfolio",
+      captionSub: "ReactJS project",
     },
   ],
 
@@ -46,7 +52,7 @@ let resumeData = {
       projectTitle: "Landing page",
       projectDetails:
         "Simple landing page using HTML + SCSS using spotify embeded styles.",
-      projectImg: "/images/projects/Project1.png",
+      projectImg: "./images/projects/Project1.png",
       code: "https://github.com/SalgueroAgus/proyecto-1",
       liveDemo: "http://salgueroagustinproyecto1.atwebpages.com/",
     },
@@ -55,7 +61,7 @@ let resumeData = {
       projectTitle: "Giphy",
       projectDetails:
         "Created using Giphy API, vanilla JS, SCSS. Dark mode compatible, you can also create new gifs via webcam, enjoy!",
-      projectImg: "/images/projects/Project2.png",
+      projectImg: "./images/projects/Project2.png",
       code: "https://github.com/SalgueroAgus/proyecto-2",
       liveDemo: "https://salgueroagustin.000webhostapp.com/",
     },
@@ -63,10 +69,19 @@ let resumeData = {
       modalId: "projectModal3",
       projectTitle: "Delilah Resto",
       projectDetails:
-        "Bckend Only using NodeJS, MySQL and Express. Online ordering system for a restaurant. REST API that allows users to register, cancel, modify and obtain information about a data structure that a customer could consume.",
-      projectImg: "/images/projects/Project3.png",
+        "Backend Only using NodeJS, MySQL and Express. Online ordering system for a restaurant. REST API that allows users to register, cancel, modify and obtain information about a data structure that a customer could consume.",
+      projectImg: "./images/projects/Project3.png",
       code: "https://github.com/SalgueroAgus/Delilah-resto",
       liveDemo: "Coming SOON",
+    },
+    {
+      modalId: "projectModal4",
+      projectTitle: "Portfolio",
+      projectDetails:
+        "Created using ReactJS",
+      projectImg: "./images/projects/Project4.png",
+      code: "https://github.com/SalgueroAgus/Portfolio",
+      liveDemo: "https://salgueroagus.github.io/Portfolio/",
     },
   ],
 };
